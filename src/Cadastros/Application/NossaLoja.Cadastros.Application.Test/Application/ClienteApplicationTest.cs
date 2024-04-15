edf6f0978cb5a29330def0dc0e0542e9bb3b1f12
MarcioPinheiroVarjao <40298827+MarcioPinheiroVarjao@users.noreply.github.com>
2024-04-15 09:25:04 -0300

    feat: Implementação Nossa Loja

---
+++ src/Cadastros/Application/NossaLoja.Cadastros.Application.Test/Application/ClienteApplicationTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NossaLoja.Cadastros.Application.Application;
+
+namespace NossaLoja.Cadastros.Application.Test.Application
+{
+    [TestClass]
+    public class ClienteApplicationTest
+    {
+        [TestMethod]
+        public void Clientes_SomaUmMaisUm_Sucesso()
+        {
+            var clienteApplication = new ClienteApplication();
+        }
+    }
+}
---
+++ src/Cadastros/Application/NossaLoja.Cadastros.Application/Application/ClienteApplication.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NossaLoja.Cadastros.Application.Application
+{
+    public class ClienteApplication
+    {
+        public int SomaMaisUm()
+        {
+            return 1 + 1;
+        }
+
+    }
+}
